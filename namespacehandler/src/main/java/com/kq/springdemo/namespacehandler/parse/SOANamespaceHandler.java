169cd9026163fbc2cbd3afe8e3af48e8e03ad609
king <kongq1983@qq.com>
2019-01-06 23:46:54 +0800

    add namespacehandler

---
+++ namespacehandler/src/main/java/com/kq/springdemo/namespacehandler/parse/SOANamespaceHandler.java
@@ -0,0 +1,28 @@
+package com.kq.springdemo.namespacehandler.parse;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+import java.rmi.registry.Registry;
+
+/**
+ * SOANamespaceHandler
+ *
+ * @author kq
+ * @date 2019/1/6
+ */
+public class SOANamespaceHandler extends NamespaceHandlerSupport {
+
+    public void init() {
+//        registerBeanDefinitionParser("registry",
+//                new RegistryBeanDefinitionParse(Registry.class));
+//        registerBeanDefinitionParser("protocol",
+//                new ProtocolBeanDefinitionParse(Protocol.class));
+//        registerBeanDefinitionParser("reference",
+//                new ReferenceBeanDifinitionParse(Reference.class));
+//        registerBeanDefinitionParser("service", new ServiceBeanDefinitionParse(
+//                Service.class));
+
+    }
+
+
+}
